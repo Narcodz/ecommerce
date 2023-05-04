09eac9807d1639a9d5b58516841039cd81bf090d
Narcodz <naziknassar@gmail.com>
2023-05-04 22:19:27 +0530

    Completeion of server, mongo DB, hash password, API register, react first setup, nav development

---
+++ client/src/pages/Pagenotfound.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Layout from '../components/Layout/Layout'
+import { Link } from 'react-router-dom'
+
+function Pagenotfound() {
+  return (
+    <Layout>
+      <div className='pnf'>
+        <h1 className='pnf-title'>Page Not Found</h1>
+        <h2 className='pnf-heading'>Ooops ! Page Not Found</h2>
+        <Link className='pnf-btn' to="/">Go Back</Link>
+      </div>
+
+    </Layout>
+
+  )
+}
+
+export default Pagenotfound
