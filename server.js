bd7566083ee527b2cdb00e0a149b1474cc475117
Narcodz <naziknassar@gmail.com>
2023-06-15 12:07:32 +0530

    updated

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
 // Configure env
 dotenv.config();
 
